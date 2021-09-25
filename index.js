e2d849c5d3e8fe802a8d86508b018897fbc0b810
Khoa Tran <kttran2320@gmail.com>
2021-09-25 14:14:46 -0400

    edit port name

--- index.js
+++ index.js
@@ -12,4 +12,4 @@ app.get("/", (req, res) => {
     res.send("Hello wordl!")
 })
 
-app.listen(PORT, () => console.log("Listening on port 3000"));
+app.listen(PORT, () => console.log("Listening on port " + PORT));
