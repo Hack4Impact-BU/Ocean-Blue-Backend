37a55f89549944d32d067c15f613c2c597a158cf
louisg1337 <louisgrassi1337@gmail.com>
2022-02-18 11:19:29 -0500

    basic username password authentication
    Implemented a basic user authentication system with mongo + bcrypt.

--- index.js
+++ index.js
@@ -2,11 +2,23 @@
 const express = require('express')
 const cors = require("cors");
 const app = express();
+<<<<<<< Updated upstream
 app.use(cors())
 app.use(express.json())
+=======
+require('dotenv').config();
+app.use(cors())
+app.use(express.json())
+// password encryption
+const bcrypt = require('bcrypt');
+// jwt token
+const jwt = require("jsonwebtoken");
+
+>>>>>>> Stashed changes
 const PORT = process.env.PORT || 3000;
 //imports
 
+<<<<<<< Updated upstream
 //routes
 app.get("/", (req, res) => {
     res.send("Welcome Ocean Blue engineers!")
