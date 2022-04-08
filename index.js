624e38a648c83ae3e4802c56204b40944c686cab
Khoa Tran <kttran2320@gmail.com>
2022-04-08 12:52:49 -0400

    db

--- index.js
+++ index.js
@@ -26,7 +26,9 @@ const jwt = require("jsonwebtoken");
 const PORT = process.env.PORT || 3000;
 
 //connect to Azure Cosmos DB through mongoose
-mongoose.connect("mongodb://"+process.env.COSMOSDB_HOST+":"+process.env.COSMOSDB_PORT+"/"+process.env.COSMOSDB_DBNAME+"?ssl=true&retrywrites=false&maxIdleTimeMS=120000&replicaSet=globaldb", {
+const URI = "mongodb://"+process.env.COSMOSDB_HOST+":"+process.env.COSMOSDB_PORT+"/"+process.env.COSMOSDB_DBNAME+"?ssl=true&retrywrites=false&maxIdleTimeMS=120000&replicaSet=globaldb";
+console.log(URI)
+mongoose.connect(URI, {
    auth: {
      username: process.env.COSMOSDB_USER,
      password: process.env.COSMOSDB_PASSWORD
