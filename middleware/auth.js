f0be75d462a2f7eb12c3f7d334ee0cbea4fed2c7
louisg1337 <louisgrassi1337@gmail.com>
2022-04-21 16:13:09 -0400

    added volunteers to retrieveEvents

--- middleware/auth.js
+++ middleware/auth.js
@@ -4,8 +4,6 @@ const config = process.env;
 
 const verifyToken = (req, res, next) => {
   const token = req.headers.token;
-  console.log(req.headers)
-  console.log(req.body)
 
   if (!token) {
     return res.status(403).send("A token is required for authentication");
